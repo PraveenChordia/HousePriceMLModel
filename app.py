77b6d0c3cf52439d14df0b4da1bc541d015db437
Praveen Kumar Chordia <45945069+PraveenChordia@users.noreply.github.com>
2021-07-20 19:37:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template
+import util
+__name__ = 'main'
+app = Flask(__name__)
+print(app)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+    balc = int(request.form['balcony'])
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath, balc)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == 'main':
+    util.load_saved_artifacts()
+    app.run(debug=True)
